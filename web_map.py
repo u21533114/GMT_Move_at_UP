e32285f143967cdc7dd35aa27db6efa11c9a8133
u21533114 <130218490+u21533114@users.noreply.github.com>
2023-08-17 10:19:38 +0200

    Update web_map.py

--- web_map.py
+++ web_map.py
@@ -1,1 +1,11 @@
-#python code here
+#import math
+#import pandas as pd
+import streamlit as st
+#import numpy as np
+#import geopandas as gpd
+#import matplotlib.pyplot as plt
+#import altair as alt
+#import folium
+#from streamlit_folium import folium_static
+
+st.write('# Move@UP')
